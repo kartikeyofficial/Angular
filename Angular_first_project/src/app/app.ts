a5c9fc8bb71309db324345337d18b2811c50eb47
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-06-25 22:17:49 +0530

    my first Angular Project

---
+++ Angular_first_project/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+
+@Component({
+  selector: 'app-root',
+  imports: [Login],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Angular_first_project');
+  name= "Kartikey";
+  sum= 6+7;
+  email='rajputkartikey10@gmail.com'
+  getNumber(a:number,b:number){
+      return a+b;
+  }
+}
