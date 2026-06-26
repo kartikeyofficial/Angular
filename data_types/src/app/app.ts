376a7010413932b94154acba17ccf6bc227701d1
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-06-26 23:24:29 +0530

    Data Types

---
+++ data_types/src/app/app.ts
@@ -0,0 +1,31 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  data:number|string|boolean = 20
+
+  updateData(val:string){
+    this.data=val;
+    console.log(this.sum(10,20))
+  }
+  editData(){
+    this.data=true;
+  }
+  
+  
+  sum(num1:number,num2:number){
+    return num1+num2;
+  }
+
+  handleEvent(event:PointerEvent){
+    console.log(event);
+    
+  }
+
+}
