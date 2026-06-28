85f9d8888dc3d31d821e29062f7b5f941554e465
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-06-28 23:02:23 +0530

    Computed Signals

---
+++ Signal/src/app/app.ts
@@ -0,0 +1,31 @@
+import { Component, effect, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ComputedSignals } from './computed-signals/computed-signals';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,ComputedSignals],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+ data = 10;
+ count = signal(0);
+ updateData(){
+  this.data++;
+ }
+
+constructor(){
+  effect(()=>{
+    console.log("this is count: ",this.count());
+    if(this.count()==10){
+      this.count.set(0);
+    }
+    
+  })
+}
+
+ updateCount(){
+  this.count.set(this.count()+1)
+ }
+}
---
+++ Signal/src/app/computed-signals/computed-signals.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'computed-signals',
+  imports: [],
+  templateUrl: './computed-signals.html',
+  styleUrl: './computed-signals.css',
+})
+export class ComputedSignals {}
