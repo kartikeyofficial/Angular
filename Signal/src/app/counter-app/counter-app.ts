5b0dc5db64fdb5c59f42784fc6d057cc17a0a4c4
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-06-30 23:08:18 +0530

    Counter App

--- Signal/src/app/counter-app/counter-app.ts
+++ Signal/src/app/counter-app/counter-app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'counter-app',
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './counter-app.html',
   styleUrl: './counter-app.css',
 })
-export class CounterApp {}
+export class CounterApp {
+  counter:WritableSignal<number> = signal<number>(0);
+
+
+  increament(){
+    this.counter.update((val)=>val+1)
+  }
+  
+  decreament(){
+    if(this.counter()>0){
+    this.counter.update((val)=>val-1)
+  }
+  }
+  reset(){
+   this.counter.set(0);
+  }
+}
+
+
+
