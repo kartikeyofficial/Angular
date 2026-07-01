6e63af2d0d72818b2790096d6a0ca898ab616709
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-07-02 01:21:57 +0530

    Control Flow if,else,toggle

--- Signal/src/app/control-flow-statemets/control-flow-statemets.ts
+++ Signal/src/app/control-flow-statemets/control-flow-statemets.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Effect } from '../effect/effect';
+import { every } from 'rxjs';
 
 @Component({
   selector: 'control-flow-statemets',
@@ -9,10 +10,15 @@ import { Effect } from '../effect/effect';
 })
 export class ControlFlowStatemets {
   isLogin = signal(true);
-
+  show = signal(true);
+  status = signal('progress')
 
   handleLogin(status:boolean){
     this.isLogin.set(status)
     
+  }
+  handleStatus(event:Event){
+    let target = event.target as HTMLSelectElement;
+    this.status.set(target.value)
   }
 }
