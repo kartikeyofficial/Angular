1ed83b0317a88dcf169a12e4d7bd570f539ccf3b
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-09-08 23:56:19 +0530

    Event Binding

--- angular-21/src/app/app.ts
+++ angular-21/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
 import { Interpolation } from './interpolation/interpolation';
+import { Binding } from './binding/binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Interpolation,Login],
+  imports: [RouterOutlet,Interpolation,Login,Binding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ angular-21/src/app/binding/binding.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'binding',
+  imports: [],
+  templateUrl: './binding.html',
+  styleUrl: './binding.css',
+})
+export class Binding {
+  callMe(){
+    alert("Hello Angular 21")
+  }
+  count = 0;
+  counter(action:String){
+    if (action=='minus'){
+      this.count>0 && this.count--;
+    }else {
+      this.count++;
+    }
+  }
+}
